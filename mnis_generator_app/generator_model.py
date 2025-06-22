6f4fd59af559e66fb015d212f00d09a7ceb5a7e3
Danilo_Carreira <101296860+Y0rkShire@users.noreply.github.com>
2025-06-22 13:25:47 -0300

    Update generator_model.py

--- mnis_generator_app/generator_model.py
+++ mnis_generator_app/generator_model.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
+        self.label_emb = nn.Embedding(10, 10)
         self.model = nn.Sequential(
-            nn.Linear(100, 128),
-            nn.ReLU(True),
+            nn.Linear(100 + 10, 128),
+            nn.ReLU(),
             nn.Linear(128, 784),
             nn.Tanh()
         )
 
-    def forward(self, z):
-        out = self.model(z)
-        return out.view(-1, 1, 28, 28)
+    def forward(self, z, labels):
+        label_input = self.label_emb(labels)
+        input = torch.cat((z, label_input), -1)
+        output = self.model(input)
+        return output.view(-1, 1, 28, 28)
